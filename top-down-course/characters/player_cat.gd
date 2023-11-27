cd0a23ccc5cefd641e781cc1d3297d008bcfcd48
Mariana Moreira <mariana.moreira@hydradev.io>
2023-11-27 19:17:11 +0100

    add tile collisions

--- top-down-course/characters/player_cat.gd
+++ top-down-course/characters/player_cat.gd
@@ -7,34 +7,32 @@ extends CharacterBody2D
 @onready var state_machine = animation_tree.get("parameters/playback")
 
 func _ready():
-	update_animation_parameters(starting_direction)
+	update_animation_direction(starting_direction)
 
 func _physics_process(_delta):
-	# get input direction
-	var input_direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+	var input_direction : Vector2 = Vector2.ZERO
 	
-	update_animation_parameters(input_direction)
+	input_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_direction.y = Input.get_action_strength("down") - Input.get_action_strength("up") 
 	
-	# update velocity
+	input_direction = input_direction.normalized()
+	
+	update_animation_direction(input_direction)
+	
 	velocity = input_direction * move_speed
 	
-	# move and slide function uses velocity of character body to move character on map
 	move_and_slide()
 	
-	pick_new_state()
+	set_new_state()
 	
-func update_animation_parameters(move_input : Vector2):
-	# don't change animation parameters if there is no move input
-	if(move_input != Vector2.ZERO):
-		animation_tree.set("parameters/idle/blend_position", move_input)
-		animation_tree.set("parameters/walk/blend_position", move_input)
+func update_animation_direction(move_input : Vector2):
+	if move_input != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", move_input)
+		animation_tree.set("parameters/Walk/blend_position", move_input)
 
 # choose state based on what is happening with the player
-func pick_new_state():
-	if(velocity != Vector2.ZERO):
-		state_machine.travel("walk")
+func set_new_state():
+	if(velocity == Vector2.ZERO):
+		state_machine.travel("Idle")
 	else:
-		state_machine.travel("idle")
+		state_machine.travel("Walk")
