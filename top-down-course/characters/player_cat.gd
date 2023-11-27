189666df52d978794a71200d1eceee6331b01e1c
Mariana Moreira <mariana.moreira@hydradev.io>
2023-11-27 18:18:44 +0100

    add player movement

--- top-down-course/characters/player_cat.gd
+++ top-down-course/characters/player_cat.gd
@@ -1,6 +1,13 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100
+@export var starting_direction : Vector2 = Vector2(0, 1)
+
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+
+func _ready():
+	update_animation_parameters(starting_direction)
 
 func _physics_process(_delta):
 	# get input direction
@@ -9,9 +16,25 @@ func _physics_process(_delta):
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 	
+	update_animation_parameters(input_direction)
+	
 	# update velocity
 	velocity = input_direction * move_speed
 	
 	# move and slide function uses velocity of character body to move character on map
 	move_and_slide()
 	
+	pick_new_state()
+	
+func update_animation_parameters(move_input : Vector2):
+	# don't change animation parameters if there is no move input
+	if(move_input != Vector2.ZERO):
+		animation_tree.set("parameters/idle/blend_position", move_input)
+		animation_tree.set("parameters/walk/blend_position", move_input)
+
+# choose state based on what is happening with the player
+func pick_new_state():
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("walk")
+	else:
+		state_machine.travel("idle")
