b82c61ac3e3c54a3c795ebd167590f786dcc6706
Mariana Moreira <mariana.moreira@hydradev.io>
2023-11-28 19:35:41 +0100

    fix player movement

--- shooter/characters/player.gd
+++ shooter/characters/player.gd
@@ -1,14 +1,20 @@
 extends CharacterBody2D
 
+@onready var sprite = $Sprite2D
+
 @export var speed : float = 200.0
+
+
+func _ready():
+	sprite.rotation_degrees = 90
 
 
 func _physics_process(_delta):
 	var direction : Vector2 = Vector2.ZERO
-	
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up") 
 	
 	velocity = direction * speed
 	
+	look_at(get_global_mouse_position())
 	move_and_slide()
